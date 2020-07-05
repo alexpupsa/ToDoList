95158d5b9607e885c9b2c7ba1a78f6617f1aa32b
Alexandru Pupsa <pupsaa@live.com>
2020-07-05 22:41:59 +0300

    Run migrations before application start

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using ToDoList.Model;
+using ToDoList.Utils;
 
 namespace ToDoList
 {
@@ -13,7 +9,7 @@ namespace ToDoList
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().MigrateDatabase<ToDoContext>().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
